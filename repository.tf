316d6e783fdb12865a73c747c19c421cf128f7e4
RalphBrynard <ralph@thebrynards.com>
2022-08-23 12:12:55 -0500

    Adding provider and repo files

---
+++ repository.tf
@@ -0,0 +1,14 @@
+resource "github_repository" "github_management" {
+  name = "github-management"
+  description = "Terraform based repository to manage all our GitHub repositories"
+
+  private = false
+  has_issues = true
+  has_wiki = true
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  auto_init = false
+  license_template = "mit"
+  topics = ["config", "terraform"]
+}
