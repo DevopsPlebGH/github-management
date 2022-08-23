3bbe16c568aecb54d807d1d79462bced8360e88c
RalphBrynard <ralph@thebrynards.com>
2022-08-23 12:54:07 -0500

    commit on main

--- repository.tf
+++ repository.tf
@@ -2,7 +2,7 @@ resource "github_repository" "github_management" {
   name        = "github-management"
   description = "Terraform based repository to manage all our GitHub repositories"
 
-  visibility         = false
+  visibility         = "public"
   has_issues         = true
   has_wiki           = true
   allow_merge_commit = false
