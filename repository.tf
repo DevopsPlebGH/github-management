c7e6284ea64b8e8bd557f29ebe42e496a3f21d95
Ralph Brynard <ralph@thebrynards.com>
2022-08-23 12:55:00 -0500

    branch protection (#3)

--- repository.tf
+++ repository.tf
@@ -11,3 +11,20 @@ resource "github_repository" "github_management" {
   auto_init          = false
   topics             = ["config", "terraform"]
 }
+
+resource "github_branch_protection" "team_baseline_config" {
+  repository_id    = github_repository.github_management.name
+  pattern          = "main"
+  enforce_admins   = false
+  allows_deletions = true
+
+  required_status_checks {
+    strict   = true
+    contexts = ["atlas/mononoke/github-management", ]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = false
+  }
+}
